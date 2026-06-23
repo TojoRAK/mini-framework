74fc2aa3b1ae4ccca0a4dc082894eb5c73094eb5
TojoRAK <tojomarofitahiana@gmail.com>
2026-06-23 10:27:32 +0300

    [feat] methodes annotées

--- src/main/java/framework/utils/AnnotationFinder.java
+++ src/main/java/framework/utils/AnnotationFinder.java
@@ -1,25 +1,39 @@
 package framework.utils;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import framework.annotation.Url;
 
 public class AnnotationFinder {
-    
-    public static List<Class<?>> findClassWithAnnotation(Class<?extends Annotation> annotation, String packageName) throws Exception{
+
+    public static List<Class<?>> findClassWithAnnotation(Class<? extends Annotation> annotation, String packageName)
+            throws Exception {
         List<Class<?>> retour = new ArrayList<>();
 
-        for(Class<?> clazz : ClassFinder.findClasses(packageName)){
-            if(clazz.isAnnotationPresent(annotation)){
+        for (Class<?> clazz : ClassFinder.findClasses(packageName)) {
+            if (clazz.isAnnotationPresent(annotation)) {
                 retour.add(clazz);
             }
         }
-
         return retour;
-
-        
     }
 
+    public static Map<String, Method> findUrls(Class<?> clazz){
+        Map<String, Method> retour = new HashMap<>();
+        Method[] methods = clazz.getMethods();
 
+        for(Method method : methods){
+            if(method.isAnnotationPresent(Url.class)){
+                Url annotation = (Url) method.getAnnotation(Url.class);
+                retour.put(annotation.url(),method);
+            }
+        }
+        return retour;
+    }
 
 }
