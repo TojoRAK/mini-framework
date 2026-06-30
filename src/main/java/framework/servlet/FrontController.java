bdd9fce951e675986cfa253e17325df12b484640
TojoRAK <tojomarofitahiana@gmail.com>
2026-06-30 11:37:30 +0300

    [feat] utilisattion de ServletContextListener

--- src/main/java/framework/servlet/FrontController.java
+++ src/main/java/framework/servlet/FrontController.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,25 +16,26 @@ import javax.servlet.http.HttpServletResponse;
 
 import framework.annotation.Controller;
 import framework.utils.AnnotationFinder;
+import framework.utils.ApplicationListener;
 import framework.utils.UrlMethod;
 
 public class FrontController extends HttpServlet {
 
-    private List<String> listController = new ArrayList<>();
-    private Map<UrlMethod, Method> urlControllers = new HashMap<>();
+    // private List<String> listController = new ArrayList<>();
+    // private Map<UrlMethod, Method> urlControllers = new HashMap<>();
 
-    @Override
-    public void init() throws ServletException {
-        String packageName = getServletConfig().getInitParameter("controller");
-        try {
-            for (Class<?> clazz : AnnotationFinder.findClassWithAnnotation(Controller.class, packageName)) {
-                listController.add(clazz.getName());
-                AnnotationFinder.findUrls(clazz, urlControllers);
-            }
-        } catch (Exception e) {
-            throw new ServletException(e.getMessage());
-        }
-    }
+    // @Override
+    // public void init() throws ServletException {
+    //     String packageName = getServletConfig().getInitParameter("controller");
+    //     try {
+    //         for (Class<?> clazz : AnnotationFinder.findClassWithAnnotation(Controller.class, packageName)) {
+    //             listController.add(clazz.getName());
+    //             AnnotationFinder.findUrls(clazz, urlControllers);
+    //         }
+    //     } catch (Exception e) {
+    //         throw new ServletException(e.getMessage());
+    //     }
+    // }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -50,13 +52,19 @@ public class FrontController extends HttpServlet {
         String uri = req.getRequestURI().substring(req.getContextPath().length());
         resp.setContentType("text/plain");
         try {
+            ServletContext context = getServletContext();
             String method = req.getMethod();
-
+            Map<UrlMethod, Method> urlControllers= (Map<UrlMethod,Method>) context.getAttribute("urlControllers"); 
+            
+            List<String> listController = (List<String>) context.getAttribute("listController");
+            
+            
             UrlMethod urlMethod = new UrlMethod(uri, method);
             PrintWriter out = resp.getWriter();
             out.println("Framework Personnalisé");
             out.println("URL : " + uri);
 
+            out.println(listController.size());
             Method correspondant = urlControllers.get(urlMethod);
 
             for (String controller : listController) {
@@ -83,7 +91,7 @@ public class FrontController extends HttpServlet {
             }
 
         } catch (IOException e) {
-            // e.printStackTrace();
+            e.printStackTrace();
         }
 
     }
