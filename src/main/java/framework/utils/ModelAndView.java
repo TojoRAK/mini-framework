a36504dd3b467e4fcf2ad9e4f7850a0ff8388276
TojoRAK <tojomarofitahiana@gmail.com>
2026-07-10 12:09:44 +0300

    [feat] fonction addObject

--- src/main/java/framework/utils/ModelAndView.java
+++ src/main/java/framework/utils/ModelAndView.java
@@ -1,15 +1,19 @@
 package framework.utils;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ModelAndView {
     private String view ;
-    private Map<String, Object> values;
+    private Map<String, Object> values = new HashMap<>();
     public String getView() {
         return view;
     }
     public void setView(String view) {
         this.view = view;
+    }
+    public void addObject(String key,Object o){
+        values.put(key, o);
     }
     public Map<String, Object> getValues() {
         return values;
